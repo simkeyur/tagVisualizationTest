28aca13af7cc382d3034444284862f538eac1bbc
shamikulamin <shamikulamin@gmail.com>
2015-04-30 11:05:25 +0400

    Comm
    Comm

--- tagVisualizationTest/SurfaceWindow1.xaml.cs
+++ tagVisualizationTest/SurfaceWindow1.xaml.cs
@@ -17,6 +17,7 @@ using Microsoft.Surface.Presentation;
 using Microsoft.Surface.Presentation.Controls;
 using Microsoft.Surface.Presentation.Input;
 using System.Windows.Media.Animation;
+using System.Collections;
 
 namespace tagVisualizationTest
 {
@@ -131,12 +132,12 @@ namespace tagVisualizationTest
 
         private void bet_button_Click(object sender, RoutedEventArgs e)
         {
-            Console.WriteLine("Bet");
+           // Console.WriteLine("Bet");
         }
 
         private void stand_button_click(object sender, RoutedEventArgs s)
         {
-            Console.WriteLine("Stand");
+          //  Console.WriteLine("Stand");
             imgArray[0] = img1;
             imgArray[1] = img2;
             imgArray[2] = img3;
@@ -149,7 +150,7 @@ namespace tagVisualizationTest
             imgArray[9] = img10;
 
             string WhiteSpace = " ";
-            string result = "bet50";
+            string result = "M";
             string binary;
             char[] binArray;
             string transferString = result + WhiteSpace;
@@ -169,31 +170,89 @@ namespace tagVisualizationTest
         {
             int binLength = binary.Length - 1;
             
-            Console.WriteLine("HERE");
+          //  Console.WriteLine("HERE");
+        //    Console.WriteLine(binary);
             TimeSpan fadeInTime = new TimeSpan(0, 0, 0, 0, 500);
             TimeSpan fadeOutTime = new TimeSpan(0, 0, 0, 0, 500);
             var fadeInAnimation = new DoubleAnimation(0d, fadeInTime);
             var fadeOutAnimation = new DoubleAnimation(1d, fadeOutTime);
-            fadeInAnimation.RepeatBehavior = new RepeatBehavior(count: 10);
-
+          //  fadeInAnimation.RepeatBehavior = new RepeatBehavior(count: 1);
+            ArrayList list = new ArrayList();
 
             for (int i = 0; i < binLength; i++)
             {
-                    fadeOutAnimation.Completed += (o, e) =>
+                if (binary[i] == '1')
+                {
+                    list.Add(i);
+                }
+            }
+
+           
+
+            fadeOutAnimation.Completed += (o, e) =>
+            {
+             //   int count=list.Count-1;
+              //  while (count >= 0)
+              //  {
+                for (int i = 0; i < binLength; i++)
+                {
+                    if (binary[i] == '1')
+                    {
+                        imgArray[i].Margin = new Thickness(600, 100 + i * 50, 0, 0);
+                        imgArray[i].BeginAnimation(Image.OpacityProperty, fadeInAnimation);
+
+                    }
+                }
+                  //  count = count - 1;
+               // }
+            };
+            int count2 = list.Count - 1;
+          //  while (count2 >= 0)
+          //  {
+            for (int i=0; i < binLength; i++)
+            {
+                if (binary[i] == '1')
+                {
+                    imgArray[i].Margin = new Thickness(600, 100 + i * 50, 0, 0);
+                    imgArray[i].BeginAnimation(Image.OpacityProperty, fadeOutAnimation);
+
+                }
+            }
+
+              /*  Console.WriteLine((int)list[count2]);
+                imgArray[(int)list[count2]].Margin = new Thickness(600, 100 + (int)list[count2]*50, 0, 0);
+                imgArray[(int)list[count2]].BeginAnimation(Image.OpacityProperty, fadeOutAnimation);
+                count2 = count2 - 1;*/
+         //   }
+           // image.BeginAnimation(Image.OpacityProperty, fadeOutAnimation);
+           
+
+          /*  for (int i = 0; i < binLength; i++)
+            {
+                if (binary[i] == '1')
+                {
+                    list.Add(i);
+                }
+                 /*   fadeOutAnimation.Completed += (o, e) =>
                     {
                         if (binary[i] == '1')
                         {
-                            imgArray[i].Margin = new Thickness(1600, 300 + i * 60, 0, 0);
+                            imgArray[i].Margin = new Thickness(600, 300 + i * 60, 0, 0);
                             imgArray[i].BeginAnimation(Image.OpacityProperty, fadeInAnimation);
                         }
-                    };
-                    if (binary[i] == '1')
-                    {
-                        imgArray[i].Margin = new Thickness(1600, 300 + i * 60, 0, 0);
-                        imgArray[i].BeginAnimation(Image.OpacityProperty, fadeOutAnimation);
-                    }
-            }
+                    };*/
+           /*     if (binary[i] == '1')
+                {
+                    imgArray[i].Margin = new Thickness(600, 300 + i * 60, 0, 0);
+                    imgArray[i].BeginAnimation(Image.OpacityProperty, fadeOutAnimation);
+                }
+             
+              
+            }*/
+           
         }
+
+       
 
         public static string ConvertToBinary(char asciiString)
         {
